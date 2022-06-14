99ae191688643faf99bee9a068e718ca160d7f2f
BeemStream <beemstreamofficial@gmail.com>
2022-06-15 00:09:41 +0100

    feat: implement list games for game manager

--- scrypto/radichess/src/chess.rs
+++ scrypto/radichess/src/chess.rs
@@ -1,46 +1,98 @@
 //! TODO:
 //! 1. Add a way for the game to end if one of the players takes too long to make a move.
 
-use scrypto::prelude::*;
 use crate::{
+    board::{Board, Fen},
     coordinate::Coordinate,
-    board::{Board, Fen}, 
-    piece::Team, 
+    piece::Team,
 };
+use scrypto::prelude::*;
+use serde::Serialize;
 
 /// Represents a chess player (old)
 #[derive(NonFungibleData)]
 struct Player {
     /// Represents the team of the player.
-    team: Team
+    team: Team,
 }
 
-blueprint!{
+#[derive(Debug, Describe, Encode, Decode, TypeId, Serialize)]
+pub enum Status {
+    Awaiting,
+    InProgres,
+    Finished
+}
+
+#[derive(Debug, Describe, Encode, Decode, TypeId, Serialize)]
+pub enum Outcome {
+    Winner(String),
+    Draw,
+    Undecided
+}
+
+blueprint! {
     /// Represents a game of chess and the required state variables.
     pub struct Chess {
         /// The resource address of the resource used for the tracking of players.
-        player1_id: NonFungibleId,
-        player2_id: Option<NonFungibleId>,
+        pub player1_id: NonFungibleId,
+        pub player2_id: Option<NonFungibleId>,
+        player1_team: Team,
+        player2_team: Option<Team>,
 
         /// The epoch in which the last move was made by the players.
         last_move_epoch: u64,
 
         /// The board object which holds the board state and controls the rules of the game
-        board: Board
+        board: Board,
     }
 
     impl Chess {
-        /// instantiate (integrated with RadiChess)
-
         pub fn instantiate(player1: NonFungibleId) -> ComponentAddress {
             let access_rules = AccessRules::new().default(AccessRule::AllowAll);
+            let board = Board::new();
 
             Self {
                 player1_id: player1,
-                board: Board::new(),
+                player1_team: Team::Black,
+                board,
                 player2_id: None,
-                last_move_epoch: Runtime::current_epoch()
-            }.instantiate().add_access_check(access_rules).globalize()
+                player2_team: None,
+                last_move_epoch: Runtime::current_epoch(),
+            }
+            .instantiate()
+            .add_access_check(access_rules)
+            .globalize()
         }
+
+        pub fn get_players(&self) -> (NonFungibleId, Option<NonFungibleId>) {
+            (self.player1_id.clone(), self.player2_id.clone())
+        }
+
+        pub fn get_status(&self) -> Status {
+            if self.player2_id.is_none() {
+                Status::Awaiting
+            } else if self.board.winner().is_some() {
+                Status::Finished
+            } else {
+                Status::InProgres
+            }
+        }
+
+        pub fn get_outcome(&self) -> Outcome { 
+            if self.board.winner().is_some() {
+                Outcome::Winner(self.get_player())
+            } else {
+                Outcome::Undecided
+            } 
+        }
+
+        fn get_player(&self) -> String {
+            if self.player1_team == self.board.winner().unwrap() {
+                self.player1_id.to_string()
+            } else {
+                self.player2_id.clone().unwrap().to_string()
+            }
+        }
+
     }
 }
--- scrypto/radichess/src/radichess.rs
+++ scrypto/radichess/src/radichess.rs
@@ -1,5 +1,6 @@
+use crate::chess::{Chess, Status, Outcome};
 use scrypto::prelude::*;
-use crate::chess::Chess;
+use serde::Serialize;
 
 #[derive(Debug, NonFungibleData)]
 struct RadiChessUser {
@@ -13,11 +14,47 @@ impl RadiChessUser {
     }
 }
 
+#[derive(Debug, Describe, Encode, Decode, TypeId, Serialize)]
+struct Player {
+    nickname: String,
+    elo: String
+}
+
+impl Player {
+    pub fn new(nickname: String, elo: String) -> Self {
+        Self {
+            nickname,
+            elo,
+        }
+    }
+}
+
+#[derive(Debug, Describe, Encode, Decode, TypeId, Serialize)]
+struct GameJSON {
+    game_address: String,
+    outcome: Outcome,
+    status: Status,
+    player1: Player,
+    player2: Option<Player>
+}
+
+impl GameJSON {
+    pub fn new(game_address: String, outcome: Outcome, status: Status, player1: Player, player2: Option<Player>) -> Self {
+        Self {
+            game_address,
+            outcome,
+            status,
+            player1,
+            player2,
+        }
+    }
+}
+
 blueprint! {
     struct RadiChess {
         service_auth: Vault,
         user_resource: ResourceAddress,
-        games: Vec<ComponentAddress>
+        games: Vec<ComponentAddress>,
     }
 
     impl RadiChess {
@@ -42,7 +79,7 @@ blueprint! {
             Self {
                 user_resource: radi_chess_user,
                 service_auth: Vault::with_bucket(service_auth),
-                games: vec![]
+                games: vec![],
             }
             .instantiate()
             .add_access_check(access_rules)
@@ -60,7 +97,10 @@ blueprint! {
 
         pub fn start_game(&mut self, badge: Proof) -> ComponentAddress {
             // Check the Badge to make sure this is a chess player
-            assert!(badge.resource_address() == self.user_resource, "Invalid Player Badge");
+            assert!(
+                badge.resource_address() == self.user_resource,
+                "Invalid Player Badge"
+            );
             // Now create the game and return the game component address
             let player_id = badge.non_fungible::<RadiChessUser>().id();
             let component = Chess::instantiate(player_id);
@@ -68,6 +108,69 @@ blueprint! {
             self.games.push(component);
 
             component
+        }
+
+        pub fn list_games(&self) -> String {
+            let found_games = self.games
+                .clone()
+                .into_iter()
+                .map(|c| {
+                    let component: Chess = c.clone().into();
+                    let (player1, player2) = component.get_players();
+
+                    let player1_details: RadiChessUser = borrow_resource_manager!(self.user_resource).get_non_fungible_data(&player1);
+
+                    let player2_details: Option<Player> = if player2.is_some() {
+                        let player2_details: RadiChessUser = borrow_resource_manager!(self.user_resource).get_non_fungible_data(&player2.unwrap());
+                        Some(Player::new(player2_details.name, player2_details.elo.to_string()))
+                    } else {
+                        None
+                    };
+
+                    GameJSON::new(c.to_string(), Outcome::Undecided, Status::Awaiting, Player::new(player1_details.name, player1_details.elo.to_string()), player2_details)
+                })
+                .collect::<Vec<GameJSON>>();
+
+            serde_json_wasm::to_string(&found_games).unwrap()
+        }
+
+        pub fn list_games_by_player(&self, badge: Proof) -> String {
+            let player_id = badge.non_fungible::<RadiChessUser>().id();
+
+            let found_games = self.games
+                .clone()
+                .into_iter()
+                .filter(|c| {
+                    let component: Chess = c.clone().into();
+                    let (player1, player2) = component.get_players();
+
+                    match player2 {
+                        Some(p) => {
+                            player_id == p
+                        },
+                        None => {
+                            player_id == player1
+                        },
+                    }
+                })
+                .map(|c| {
+                    let component: Chess = c.clone().into();
+                    let (player1, player2) = component.get_players();
+
+                    let player1_details: RadiChessUser = borrow_resource_manager!(self.user_resource).get_non_fungible_data(&player1);
+
+                    let player2_details: Option<Player> = if player2.is_some() {
+                        let player2_details: RadiChessUser = borrow_resource_manager!(self.user_resource).get_non_fungible_data(&player2.unwrap());
+                        Some(Player::new(player2_details.name, player2_details.elo.to_string()))
+                    } else {
+                        None
+                    };
+
+                    GameJSON::new(c.to_string(), Outcome::Undecided, Status::Awaiting, Player::new(player1_details.name, player1_details.elo.to_string()), player2_details)
+                })
+                .collect::<Vec<GameJSON>>();
+
+            serde_json_wasm::to_string(&found_games).unwrap()
         }
 
         // This is a method, because it needs a reference to self.  Methods can only be called on components

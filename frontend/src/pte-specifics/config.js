25a76ed5cb4d7a32a6b92342cd338b466cd31f67
James Wylie <9528193+jameswylie@users.noreply.github.com>
2022-07-25 18:51:19 +0100

    Added auction paths.

--- frontend/src/pte-specifics/config.js
+++ frontend/src/pte-specifics/config.js
@@ -1,7 +1,8 @@
 const config = {
 
-    wasm_path: process.env.PUBLIC_URL + '/radichess.wasm'
+    chess_wasm_path: process.env.PUBLIC_URL + '/radichess.wasm',
+    auction_wasm_path: process.env.PUBLIC_URL + '/auction.wasm',
 
-}
+};
 
 export default config;
